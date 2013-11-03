d422d481ec9ad80b168d9f5d4777aad08f554cea
Kevin Stanton <stantonk@gmail.com>
2013-11-03 15:41:34 -0600

    dicts.and.maps.scala

---
+++ src/dicts.and.maps.scala
@@ -0,0 +1,35 @@
+/*
+>>> x = {"dog": "Woof", "cat": "Meow"}
+>>> for k, v in x.items():
+...   print "%s=%s" % (k, v)
+...
+dog=Woof
+cat=Meow
+
+>>> print "the dog says " + x["dog"] + " and the cat says " + x["cat"]
+the dog says Woof and the cat says Meow
+
+>>> print x.get("duck", "quack")
+quack
+
+>>> x['fox'] = '??????'
+>>> print "what the fox say: " + x['fox']
+what the fox say: ??????
+*/
+
+var x = Map("dog" -> "Woof", "cat" -> "Meow")
+println("the dog says " + x("dog") + " and the cat says " + x("cat")) // like lists, note parens..not brackets
+// the dog says Woof and the cat says Meow
+
+for ((k, v) <- x)
+  printf("%s=%s\n", k, v)
+// dog=Woof
+// cat=Meow
+
+println(x.getOrElse("duck", "quack"))
+// quack
+
+x += ("fox" -> "??????")
+
+println("what the fox say: " + x("fox"))
+// what the fox say: ??????
