d8c8ccb1971407e9e7c5de16f40acd4f9a9049f9
Kevin Stanton <stantonk@gmail.com>
2013-11-03 15:46:57 -0600

    added functions.oneliners.scala

---
+++ src/functions.oneliners.scala
@@ -0,0 +1,16 @@
+/*
+def max2(x, y): return x if x > y else y
+
+>>> max2(17, 19)
+19
+>>> max2(19, 17)
+19
+*/
+
+// scala, function definition (brief)
+def max2(x: Int, y: Int) = if (x > y) x else y
+
+max2(17, 19)
+// res2: Int = 19
+max2(19, 17)
+// res3: Int = 19
